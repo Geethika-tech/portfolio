5a7511f6978c1f9e0cd3516576f8f7acbc4e57ef
Geethika Pidikiti <geethikapidikiti@gmail.com>
2025-10-06 17:08:01 +1100

    added changes to API

--- my-website/src/app/api/send/route.js
+++ my-website/src/app/api/send/route.js
@@ -1,18 +1,24 @@
+import { NextResponse } from "next/server";
 import { Resend } from 'resend';
-import { NextResponse } from 'next/server';
-
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
-  const { name, email, message } = await req.json();
+  const { name, email, subject, message } = await req.json();
+  const resend = new Resend(process.env.RESEND_API_KEY);
 
   try {
     await resend.emails.send({
       from: 'Portfolio Contact <onboarding@resend.dev>',
-      to: 'geethikapidikiti9@gmail.com',
-      subject: `New message from ${name}`,
-      reply_to: email,
-      text: message,
+      to: 'geethikapidikiti9@gmail.com', // your inbox
+      subject: `New message from ${name} - ${subject}`,
+      reply_to: email, // lets you directly reply to the sender
+      text: `
+You’ve received a new message from your portfolio site.
+
+👤 Name: ${name}
+📧 Email: ${email}
+📝 Message:
+${message}
+      `,
     });
 
     return NextResponse.json({ success: true });
